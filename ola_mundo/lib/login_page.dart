0448884c4ab519101c1b99c896a0f4d5b9ded858
Henrique Salazar da Silva <henriquesalazards16@hotmail.com>
2025-12-31 12:00:18 -0300

    Flutterando: Login Page (TextField)

--- ola_mundo/lib/login_page.dart
+++ ola_mundo/lib/login_page.dart
@@ -6,34 +6,51 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Material(
       child: SizedBox(
         width: double.infinity,
         height: double.infinity,
-
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-
             children: [
               TextField(
+                onChanged: (text) {
+                  email = text;
+                },
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
                   labelText: 'Email',
                   border: OutlineInputBorder(),
                 ),
               ),
-
+              SizedBox(height: 10),
               TextField(
+                onChanged: (text) {
+                  password = text;
+                },
                 obscureText: true,
                 decoration: InputDecoration(
                   labelText: 'Password',
                   border: OutlineInputBorder(),
                 ),
+              ),
+              SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: () {
+                  if (email == 'email@email.com' && password == '123') {
+                    print('Login successful');
+                  } else {
+                    print('Invalid credentials');
+                  }
+                },
+                child: Text('Entrar'),
               ),
             ],
           ),
