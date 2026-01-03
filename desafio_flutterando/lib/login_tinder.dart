8d66331d53783e07e044eeccc039956221caae3a
Henrique Salazar da Silva <henriquesalazards16@hotmail.com>
2026-01-03 13:07:57 -0300

    Flutterando #27: Desafio Layout

--- desafio_flutterando/lib/login_tinder.dart
+++ desafio_flutterando/lib/login_tinder.dart
@@ -4,10 +4,18 @@ class LoginTinder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Icon(Icons.local_fire_department, size: 100, color: Colors.red),
-        ],
+      backgroundColor: const Color.fromARGB(255, 253, 98, 87),
+
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.local_fire_department, size: 100, color: Colors.white),
+            Text('Location Changer'),
+          ],
+        ),
       ),
     );
   }
