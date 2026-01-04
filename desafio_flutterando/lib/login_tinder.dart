c62904ace854aaf65fb51c1acd06b2e2969feb94
Henrique Salazar da Silva <henriquesalazards16@hotmail.com>
2026-01-04 10:58:44 -0300

    95% completed, improving the layout code

--- desafio_flutterando/lib/login_tinder.dart
+++ desafio_flutterando/lib/login_tinder.dart
@@ -12,8 +12,75 @@ class LoginTinder extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.local_fire_department, size: 100, color: Colors.white),
-            Text('Location Changer'),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 12,
+                right: 12,
+                bottom: 14,
+                top: 12,
+              ),
+              child: Icon(
+                Icons.local_fire_department,
+                size: 150,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 8,
+                right: 8,
+                bottom: 2,
+                top: 8,
+              ),
+              child: Text(
+                'Location Changer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Text(
+              'Plugin app for Tinder',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 10,
+                right: 10,
+                bottom: 10,
+                top: 30,
+              ),
+              child: Container(
+                width: 215,
+
+                child: ElevatedButton(
+                  onPressed: () {
+                    print('Logou com Facebook');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Color.fromARGB(255, 253, 98, 87),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      'Login with Facebook',
+                      style: TextStyle(letterSpacing: 0.5),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
