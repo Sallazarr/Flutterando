82a2b24f8b79fa8721864d21452fab0a32d17085
Henrique Salazar da Silva <henriquesalazards16@hotmail.com>
2026-01-20 13:52:04 -0300

    #41 API

--- api/lib/sync_async.dart
+++ api/lib/sync_async.dart
@@ -3,8 +3,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future main() async {
-  final json = await fetch();
-  print(json);
+  final todo = await fetch();
+  print(todo.toJson());
   // var future = Future.delayed(Duration(seconds: 2));
   // future.then((value) => print('4'));
   //   print4();
@@ -22,9 +22,44 @@ Future main() async {
   //   print('4');
 }
 
-Future<Map> fetch() async {
+Future<ToDo> fetch() async {
   var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
   var response = await http.get(url);
   var json = jsonDecode(response.body);
-  return json;
+  var todo = ToDo.fromJson(json);
+  return todo;
+}
+
+// {
+//   "userId": 1,
+//   "id": 1,
+//   "title": "delectus aut autem",
+//   "completed": false
+// }
+
+class ToDo {
+  final String title;
+  final int id;
+  final int userId;
+  final bool completed;
+
+  ToDo({
+    required this.title,
+    required this.id,
+    required this.userId,
+    required this.completed,
+  });
+
+  factory ToDo.fromJson(Map json) {
+    return ToDo(
+      title: json['title'],
+      id: json['id'],
+      userId: json['userId'],
+      completed: json['completed'],
+    );
+  }
+
+  Map toJson() {
+    return {'title': title, 'id': id, 'userId': userId, 'completed': completed};
+  }
 }
